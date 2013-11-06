7d9ae52735f55608d1c3ee5ce82ed7127e8534f0
Tyler Conrad <conradt80@gmail.com>
2013-11-05 19:43:38 -0600

    Factor out some modal classes.

--- zoomback/uix/modal.py
+++ zoomback/uix/modal.py
@@ -19,16 +19,23 @@ from zoomback.util.misc import root
 class ListToggleButton(ToggleButton):
     def select(self, *args):
         self.state = 'down'
-        next(q(Window, cls=['config_modal'])).dismiss()
-        next(q(root(), cls=['selector'])).config = self.text
+        self.handle_select()
 
     def deselect(self, *args):
         self.state = 'normal'
 
-class ConfigModal(ModalView):
-    def __init__(self, **kw):
-        kw['cls'] = ['config_modal']
-        super(ConfigModal, self).__init__(**kw)
+
+class ConfigToggleButton(ListToggleButton):
+    def handle_select(self):
+        next(q(Window, cls=['config_modal'])).dismiss()
+        next(q(root(), cls=['selector'])).config = self.text
+
+
+class Modal(ModalView):
+    def __init__(self, tag, load_dir, list_item_cls, **kw):
+        kw['cls'] = [tag]
+        super(Modal, self).__init__(**kw)
+
         def converter(row_index, obj):
             return {
                 'text': obj,
@@ -39,9 +46,18 @@ class ConfigModal(ModalView):
             ListView(adapter=ListAdapter(
                 data=[
                     config.split('.')[0]
-                    for config in listdir('zoomback/config/config')],
+                    for config in listdir(load_dir)],
                 args_converter=converter,
                 selection_mode='single',
                 propagate_selection_to_data=False,
                 allow_empty_selection=True,
-                cls=ListToggleButton)))
+                cls=list_item_cls)))
+
+
+class ConfigModal(Modal):
+    def __init__(self, **kw):
+        super(ConfigModal, self).__init__(
+            tag='config_modal',
+            load_dir='zoomback/config/config',
+            list_item_cls=ConfigToggleButton, 
+            **kw)
